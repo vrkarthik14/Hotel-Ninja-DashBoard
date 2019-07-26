e0cffe6d6a6e4eca9560846d565f0f62e85d8e84
vrkarthik14 <vrkarthik14@gmail.com>
2019-07-26 19:52:18 +0530

    final commiy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,27 @@ import Amplify from "aws-amplify";
 import awsconfig from "./aws-exports";
 import { withAuthenticator } from "aws-amplify-react"; // or 'aws-amplify-react-native';
 
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
 Amplify.configure(awsconfig);
+
+//style component
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing(3),
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 650
+  }
+}));
 
 class App extends Component {
   state = { data: [] };
@@ -18,12 +38,38 @@ class App extends Component {
       this.setState({
         data: snapshot.val()
       });
+      console.log(snapshot.val());
     });
   }
 
   render() {
-    return <div>Hello</div>;
+    const classes = this.props;
+
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>CehckIn</TableCell>
+              <TableCell>CheckOut</TableCell>
+              <TableCell>Count</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {Object.keys(this.state.data).map(key => (
+              <TableRow>
+                <TableCell>{this.state.data[key]["name"]}</TableCell>
+                <TableCell>{this.state.data[key]["checkIn"]}</TableCell>
+                <TableCell>{this.state.data[key]["checkOut"]}</TableCell>
+                <TableCell>{this.state.data[key]["count"]}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
   }
 }
 
-export default withAuthenticator(App, true);
+export default withAuthenticator(withStyles(useStyles)(App), true);
